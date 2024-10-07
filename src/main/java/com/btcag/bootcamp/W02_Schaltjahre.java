2e21a49cb49b83c8ca9ae1bf4897adc086746768
STKUSHNI <stanislau.kushniarou@btc-ag.com>
2024-10-07 10:07:53 +0200

    Aufgabe Schaltjahre

---
+++ src/main/java/com/btcag/bootcamp/W02_Schaltjahre.java
@@ -0,0 +1,31 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class W02_Schaltjahre {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte geben Sie ein Jahr ein: ");
+        int jahr = sc.nextInt();
+
+        if (jahr % 4 == 0) {
+            if (jahr % 100 == 0) {
+                if (jahr % 400 == 0) {
+                    schaltjahr(jahr);
+                }
+                keinSchaltjahr(jahr);
+            }
+            schaltjahr(jahr);
+        } else {
+            keinSchaltjahr(jahr);
+        }
+    }
+
+    public static void schaltjahr(int jahr) {
+        System.out.println(jahr + " ist ein Schaltjahr");
+    }
+
+    public static void keinSchaltjahr(int jahr) {
+        System.out.println(jahr + " ist kein Schaltjahr");
+    }
+}
